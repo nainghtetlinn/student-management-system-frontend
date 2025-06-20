33171d457204acbb98d1950c0dd5de7f65c004b7
nainghtetlinn <naing.work.it@gmail.com>
2025-06-21 00:37:41 +0630

    refactor: remove unused schemas from StudentRegistrationForm steps

--- src/components/features/registration/StudentRegistrationForm/steps.tsx
+++ src/components/features/registration/StudentRegistrationForm/steps.tsx
@@ -1,11 +1,4 @@
-import {
-  acknowledgementSchema,
-  contactsSchema,
-  matriculationExamDetailsSchema,
-  parentsDetailsSchema,
-  studentDetailsSchema,
-  type TCombinedStudentRegistrationSchema,
-} from '@/validators/registration/studentRegistrationForm'
+import { type TCombinedStudentRegistrationSchema } from '@/validators/registration/studentRegistrationForm'
 
 import { AcknowledgementDetails } from './AcknowledgementDetails'
 import { ContactsDetails } from './ContactsDetails'
@@ -13,13 +6,10 @@ import { MatriculationExamDetails } from './MatriculationExamDetails'
 import { ParentsDetails } from './ParentsDetails'
 import { StudentDetails } from './StudentDetails'
 
-import { type ZodType } from 'zod'
-
 export type FormStep<T, FieldKeys = keyof T> = {
   position: number
   title: string
   component: React.ReactElement
-  schema: ZodType<unknown>
   fields: FieldKeys[]
 }
 
@@ -28,14 +18,12 @@ export const steps: FormStep<TCombinedStudentRegistrationSchema>[] = [
     position: 1,
     title: 'ကျောင်းသားကိုဃ်ရေးအချက်အလက်',
     component: <StudentDetails />,
-    schema: studentDetailsSchema,
     fields: ['courseName', 'student'],
   },
   {
     position: 2,
     title: 'တက္ကသိုလ်ဝင်တန်းအချက်အလက်',
     component: <MatriculationExamDetails />,
-    schema: matriculationExamDetailsSchema,
     fields: ['matriculationExam'],
   },
 
@@ -43,21 +31,18 @@ export const steps: FormStep<TCombinedStudentRegistrationSchema>[] = [
     position: 3,
     title: 'မိဘအချက်အလက်',
     component: <ParentsDetails />,
-    schema: parentsDetailsSchema,
     fields: ['father', 'mother'],
   },
   {
     position: 4,
     title: 'ဆက်သွယ်ရန်အချက်အလက်',
     component: <ContactsDetails />,
-    schema: contactsSchema,
     fields: ['contacts'],
   },
   {
     position: 5,
     title: '',
     component: <AcknowledgementDetails />,
-    schema: acknowledgementSchema,
     fields: ['acknowledged'],
   },
 ]
