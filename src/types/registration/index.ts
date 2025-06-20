9fe0fad73cf9a56cd9722ff44b9d7a797f34c9af
nainghtetlinn <naing.work.it@gmail.com>
2025-06-20 23:14:33 +0630

    remove unused StudentRegistrationFormSchema type from registration index

--- src/types/registration/index.ts
+++ src/types/registration/index.ts
@@ -1,12 +1,8 @@
-import { studentRegistrationFormSchema } from '@/validators/registration/studentRegistrationFormSchema'
 import { studentDetailsFormSchema } from '@/validators/registration/studentDetailsFormSchema'
 import { formalStudentRegistrationFormSchema } from '@/validators/registration/formalStudentRegistrationFormSchema'
 
 import { z } from 'zod'
 
-export type StudentRegistrationFormSchema = z.infer<
-  typeof studentRegistrationFormSchema
->
 export type StudentDetailsFormSchema = z.infer<typeof studentDetailsFormSchema>
 export type FormalStudentRegistrationFormSchema = z.infer<
   typeof formalStudentRegistrationFormSchema
